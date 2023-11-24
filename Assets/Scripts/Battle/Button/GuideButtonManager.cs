c99ebd9d91a65e721d9edafe2c91153b94438436
STUDENT\2111118 <suinaikrml@gmail.com>
2023-11-24 12:30:50 +0900

    バトル画面でガイドビューを開く処理、ポーズビュー開いたときにフォーカスがSEControlに合わない不具合あり

---
+++ Assets/Scripts/Battle/Button/GuideButtonManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GuideButtonManager : Button
+{
+    [Header("ガイドビュー")]
+    [SerializeField] private GameObject _guideView;
+    [SerializeField] private GameObject _battleDefaultForcus;
+    public bool IsOpenGuideView { get; private set; } = false;
+    private bool _isPointerDown = false;
+
+    [Header("アイコン")]
+    [SerializeField] private Sprite[] _sourceImage = new Sprite[2];
+    private Image _buttonIcon;
+
+    private const string _assignmentButton = "Y";
+
+    public override void Start()
+    {
+        _buttonIcon = this.GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        // 入力中のみガイドを表示する
+        if (Input.GetAxis(_assignmentButton) > 0 && CanInput)
+        {
+            Select();
+        }
+
+        if (Input.GetAxisRaw(_assignmentButton) == 0 && !_isPointerDown)
+        {
+            Deselect();
+            CanInput = true;
+        }
+    }
+
+    // ガイドを開く
+    public override void Select()
+    {
+        CanInput = false;
+
+        // ビューの設定
+        _guideView.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        IsOpenGuideView = true;
+
+        // ボタン画像の設定
+        _buttonIcon.sprite = _sourceImage[1];
+    }
+
+    // ガイドを閉じる
+    public override void Deselect()
+    {
+        // ビューの設定
+        _guideView.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(_battleDefaultForcus);
+        IsOpenGuideView = false;
+
+        // ボタン画像の設定
+        _buttonIcon.sprite = _sourceImage[0];
+    }
+
+    // マウスでの操作
+    public void PointerDown()
+    {
+        _isPointerDown = true;
+        Select();
+    }
+
+    public void PointerUp()
+    {
+        _isPointerDown = false;
+        Select();
+    }
+}
---
+++ Assets/Scripts/Battle/Button/PouseButtonManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PouseButtonManager : Button
+{
+    [Header("ガイドビュー")]
+    [SerializeField] private GameObject _pouseView;
+    [SerializeField] private GameObject _pouseDefaultForcus;
+    [SerializeField] private GameObject _battleDefaultForcus;
+    private bool _isOpenPouseView = false;
+
+    [Header("アイコン")]
+    [SerializeField] private Sprite[] _sourceImage = new Sprite[2];
+    private Image _buttonIcon;
+
+    private const string _assignmentButton = "X";
+
+
+    public override void Start()
+    {
+        _buttonIcon = this.GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        // 入力
+        if (Input.GetAxis(_assignmentButton) > 0 && CanInput)
+        {
+            Select();
+        }
+
+        // 入力をやめたら再入力可能
+        if (Input.GetAxisRaw(_assignmentButton) == 0)
+        {
+            CanInput = true;
+        }
+    }
+
+    // ポーズの開閉
+    public override void Select()
+    {
+        CanInput = false;
+
+        // 開く
+        if (!_isOpenPouseView)
+        {
+            // ビューの設定
+            _pouseView.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(_pouseDefaultForcus);
+            _isOpenPouseView = true;
+
+            // ボタン画像の設定
+            _buttonIcon.sprite = _sourceImage[1];
+
+            return;
+        }
+
+        // 閉じる
+        if (_isOpenPouseView)
+        {
+            // ビューの設定
+            _pouseView.SetActive(false);
+            EventSystem.current.SetSelectedGameObject(_battleDefaultForcus);
+            _isOpenPouseView = false;
+
+            // ボタン画像の設定
+            _buttonIcon.sprite = _sourceImage[0];
+
+            return;
+        }
+    }
+}
